88408b51e8ba369ecbd29afbcf5e02358943aeb1
Jake Soenneker <jake@soenneker.com>
2023-03-31 20:44:58 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Extensions.Uri.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.Uri/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.extensions.uri/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.extensions.uri/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Extensions.Uri.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.Uri/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Extensions.Uri
+### A collection of useful Uri extension methods
+
+## Installation
+
+```
+Install-Package Soenneker.Extensions.Uri
+```
---
+++ src/UriExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.Contracts;
+using Soenneker.Utils.RegexCollection;
+
+namespace Soenneker.Extensions.Uri;
+
+/// <summary>
+/// A collection of useful Uri extension methods
+/// </summary>
+public static class UriExtension
+{
+    [Pure]
+    public static System.Uri ReplaceLastSegment(this System.Uri uri, string replacement)
+    {
+        string regexResult = RegexCollection.UriLastSegment().Replace(uri.AbsoluteUri, $"$1/{replacement}$3");
+
+        var rtnUri = new System.Uri(regexResult);
+        return rtnUri;
+    }
+
+    [Pure]
+    public static System.Uri RemoveLastSegment(this System.Uri uri)
+    {
+        string regexResult = RegexCollection.UriLastSegment().Replace(uri.AbsoluteUri, "$1$3");
+
+        var rtnUri = new System.Uri(regexResult);
+        return rtnUri;
+    }
+}

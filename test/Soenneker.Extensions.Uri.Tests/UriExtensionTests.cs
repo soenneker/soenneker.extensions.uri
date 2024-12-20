78f4320225d4f928499076c398f9b53fcaca86a7
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:22 +0000

    Tests

---
+++ test/Soenneker.Extensions.Uri.Tests/UriExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Uri.Tests;
+
+public class UriExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}

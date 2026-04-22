2c8e2f3fd549a7cfe207d24a92c894b63584486f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:06 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Uri.Tests/UriExtensionTests.cs
+++ test/Soenneker.Extensions.Uri.Tests/UriExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.Uri.Tests;
 
 public class UriExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
